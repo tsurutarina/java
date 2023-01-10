6e27e4860cb5ecd30e9c0036dd98d08867796f27
tsuruta <naritakundono@gmail.com>
2023-01-10 15:00:05 +0000

    [Add]ボクシング

---
+++ exercise/Chapter05.java
@@ -0,0 +1,20 @@
+public class Chapter05 {
+  public static void main(String[] args) {
+    long l = 100000000L;
+    System.out.println(l);
+    double d = 2.1;
+    System.out.println(d);
+    boolean b = false;
+    System.out.println(b);
+
+    Float f = 5.5f;
+    System.out.println(f);
+    Integer i = 6;
+    System.out.println(i);
+    Character c = 'あ';
+    System.out.println(c);
+
+    int[] array1 = {7,8,9,10,11};
+    System.out.println(array1[3]);
+  }
+}
