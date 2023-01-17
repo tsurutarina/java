43d7a31f90e00aa5ef9afecf0000632dac7594cd
tsuruta <naritakundono@gmail.com>
2023-01-17 15:12:27 +0000

    [Add]exercise_class,method

---
+++ exercise/Car.java
@@ -0,0 +1,20 @@
+public class Car {
+  private double fuelCost;
+  private double fuelAmount;
+
+  // X
+
+  public Car(double fuelCost, double fuelAmount) {
+    this.fuelCost = fuelCost;
+    this.fuelAmount = fuelAmount;
+  }
+
+  public void move(int km) {
+    System.out.println(km + " km 走ります");
+    this.fuelAmount -= (km / fuelCost);
+  }
+
+  public double getFuelAmount() {
+    return this.fuelAmount;
+  }
+}
