443f9a9f14b12c4bc5b3daede95d0896388e3788
tsuruta <naritakundono@gmail.com>
2023-01-26 05:16:20 +0000

    [Add]抽象クラス

---
+++ Abstract01.java
@@ -0,0 +1,7 @@
+public class Abstract01 {
+  public static void main(String[] args) {
+    Dog dog = new Dog();
+    dog.eat();
+    dog.cry();
+  }
+}
---
+++ Dog.java
@@ -0,0 +1,8 @@
+public class Dog extends Animal {
+  public Dog() {
+    this.name = "犬";
+  }
+  public void cry() {
+    System.out.println(this.name + "が吠えました。ワン！ワン！");
+  }
+}
